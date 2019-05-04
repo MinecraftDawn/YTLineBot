8d4142c1757132e0b47bd280837b8dfdc21de8fe
MinecraftDawn <eric4902077@gmail.com>
2019-05-05 02:27:45 +0800

    可以從本機傳送影片

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort , send_file
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -30,15 +30,28 @@ def callback():
         abort(400)
     return 'OK'
 
+@app.route("/download/<filename>", methods=['GET'])
+def download_file(filename):
+    return send_file(app.root_path + "\\test.mp4",as_attachment=True)
+
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     userID = event.source.user_id
     
+#    ha = msg2yt(event.message.text)
+    
     message = TextSendMessage(text=event.message.text)
     
     line_bot_api.reply_message(event.reply_token, message)
     
+    video_message = VideoSendMessage(
+            original_content_url='<Your_Ngrok_https>/download/filename',
+            preview_image_url='https://i.vimeocdn.com/video/681613544_640.jpg'
+        )
+    line_bot_api.push_message(userID,video_message)
+    
+    """
     try:
         video_message = VideoSendMessage(
             original_content_url='https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4',
@@ -49,7 +62,18 @@ def handle_message(event):
         print(e.status_code)
         print(e.error.message)
         print(e.error.details)
-
+    """
+from pytube import YouTube
+def msg2yt(url):
+    try:
+        print(url)
+        yt = YouTube(url)
+        yt.streams.first().download(output_path='YT_Movies')
+        return yt.title
+    except:
+        print("YT錯誤")
+        return ""
+    
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
