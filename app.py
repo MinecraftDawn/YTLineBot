d5d66aaac841a63721835d0764eb0eeece0bcfba
MinecraftDawn <eric4902077@gmail.com>
2019-05-09 21:23:08 +0800

    可下載Youtube影片及縮圖，並上傳(功能初步完成)

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 
+import os
+
 app = Flask(__name__)
 
 # Channel Access Token
@@ -32,22 +34,29 @@ def callback():
 
 @app.route("/download/<filename>", methods=['GET'])
 def download_file(filename):
-    return send_file(app.root_path + "\\test.mp4",as_attachment=True)
+    
+    urlPath = app.root_path + '\\YT_Movies\\' + filename
+    #判斷檔案是否存在
+    if os.path.exists(urlPath):
+        return send_file(urlPath,as_attachment=True)
+    else:
+        return "檔案不存在" + urlPath
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     userID = event.source.user_id
     
-#    ha = msg2yt(event.message.text)
-    
     message = TextSendMessage(text=event.message.text)
     
-    line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(event.reply_token, message) 
+    
+    hashName = msg2yt(event.message.text)
+    
     
     video_message = VideoSendMessage(
-            original_content_url='<Your_Ngrok_https>/download/filename',
-            preview_image_url='https://i.vimeocdn.com/video/681613544_640.jpg'
+            original_content_url='https://<Your_Ngrok_Url>/download/' + str(hashName) + '.mp4',
+            preview_image_url='https://<Your_Ngrok_Url>/download/' + str(hashName) + '.png'
         )
     line_bot_api.push_message(userID,video_message)
     
@@ -63,18 +72,34 @@ def handle_message(event):
         print(e.error.message)
         print(e.error.details)
     """
-from pytube import YouTube
-def msg2yt(url):
-    try:
-        print(url)
-        yt = YouTube(url)
-        yt.streams.first().download(output_path='YT_Movies')
-        return yt.title
-    except:
-        print("YT錯誤")
-        return ""
     
-import os
+from pytube import YouTube
+import requests
+
+def msg2yt(url):
+#try:
+    print(url)
+    yt = YouTube(url)
+    print(str(hash(yt.title)))
+    fileName = str(hash(yt.title))
+    yt.streams.first().download(output_path='YT_Movies',filename=fileName)
+    
+    image = requests.get(yt.thumbnail_url)
+    
+    if image.status_code == 200:
+        with open(app.root_path + "\\YT_Movies\\" + fileName + ".png", 'wb') as f:
+            f.write(image.content)
+            f.close()
+    else:
+        print("Error")
+    del image
+    
+    return fileName
+
+#except:
+    print("YT錯誤")
+    return ""
+    
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
